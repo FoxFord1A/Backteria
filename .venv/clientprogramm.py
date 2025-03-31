61d3b6fc4ee82031d21f167802a7d622c19a7e90
Артём <artiomgoriachev@yandex.ru>
2025-03-31 18:41:01 +0300

    Основа

--- .venv/clientprogramm.py
+++ .venv/clientprogramm.py
@@ -1,19 +1,37 @@
 import socket
 import pygame
+import math
 
 pygame.init()
 WIDTH = 800
 HEIGHT = 600
+CC = (WIDTH//2, HEIGHT//2)
+radius = 50
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Бактерия")
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Настраиваем сокет
 sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)  # Отключаем пакетирование
 sock.connect(("localhost", 10000))
 run = True
-
+old = (0,0)
 while run:
     for event in pygame.event.get():
         if event == pygame.QUIT:
             run = False
-    sock.send("Пользователь Devloper отправил данные".encode())
+    if pygame.mouse.get_focused():
+        pos = pygame.mouse.get_pos()
+        vector = pos[0]-CC[0],pos[1]-CC[1]
+        lenv = math.sqrt(vector[0]**2,vector[1]**2)
+        vector = vector[0] / lenv, vector[1] / lenv
+        if lenv <= radius:
+            vector = 0,0
+        if vector != old:
+            old = vector
+            msg = f"<{vector[0]},{vector[1]}>"
+            sock.send(msg.encode())
+    data = sock.recv(1024).decode()
+    print(f"Получил: {data}")
+    screen.fill("gray")
+    pygame.draw.circle(screen, "orange", CC, radius)
+    pygame.display.update()
 pygame.quit()
--- .venv/serverprogramm.py
+++ .venv/serverprogramm.py
@@ -1,15 +1,54 @@
 import socket
 import time
+import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column,Integer,String
+from sqlalchemy.orm import sessionmaker
 
 main_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY,1)
 main_socket.bind(("localhost",10000))
 main_socket.setblocking(False)
 main_socket.listen(5)
+engine = create_engine("postgresql+psycopg2://postgres:2222@localhost/MGB")
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+s = Session()
+class Player(Base):
+  __tablename__ = "gamers"
+  id = Column(Integer, primary_key=True, autoincrement=True)
+  name = Column(String(250))
+  address = Column(String)
+  x = Column(Integer, default=500)
+  y = Column(Integer, default=500)
+  size = Column(Integer, default=50)
+  errors = Column(Integer, default=0)
+  abs_speed = Column(Integer, default=1)
+  speed_x = Column(Integer, default=0)
+  speed_y = Column(Integer, default=0)
+
+  def __init__(self, name, address):
+      self.name = name
+      self.address = address
+class LocalPlayer:
+  def __init__(self, id, name, sock, addr):
+      self.id = id
+      self.db: Player = s.get(Player, self.id)
+      self.sock = sock
+      self.name = name
+      self.address = addr
+      self.x = 500
+      self.y = 500
+      self.size = 50
+      self.errors = 0
+      self.abs_speed = 1
+      self.speed_x = 0
+      self.speed_y = 0
 
 print("Сокет создался")
 
-players = []
+players = {}
 
 
 while True:
@@ -17,7 +56,14 @@ while True:
         new_socket, addr = main_socket.accept()
         new_socket.setblocking(False)
         print("Подключился", addr)
-        players.append(new_socket)
+        player = Player("name",addr)
+        s.merge([player])
+        s.commit()
+        addr = f"({addr[0]},{addr[1]})"
+        data = s.query(Player).filter(Player.address==addr)
+        for user in data:
+            player = LocalPlayer(user.id,"name", new_socket, addr)
+            players[user.id] = player
     except BlockingIOError:
         pass
     for sock in players:
